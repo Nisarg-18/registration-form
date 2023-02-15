428dc7f3240681d22ab2ed30cb4fc259f7b9d10a
Nisarg <nisargs2001@gmail.com>
2023-02-15 17:28:17 +0530

    deployment changes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const express = require('express');
-const connectToDB = require('./config/db');
 const userRouter = require('./routes/registerRoutes');
 const path = require("path")
 
@@ -10,9 +9,6 @@ const app = express();
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
-
-// connect to DB
-connectToDB();
 
 app.use('/', userRouter);
 
--- server.js
+++ server.js
@@ -1,7 +1,25 @@
-const app = require('./app');
+const app = require("./app");
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT,()=>{
-    console.log(`Listening on http://localhost:${PORT}`);
-});
+const mongoose = require("mongoose");
+
+const URL = process.env.MONGO_URL;
+
+const connectToDB = () => {
+  mongoose.set("strictQuery", false);
+  mongoose
+    .connect(URL)
+    .then((connection) => {
+      console.log(`Connected to DB: ${connection.connection.host}`);
+      app.listen(PORT, () => {
+        console.log(`Listening on http://localhost:${PORT}`);
+      });
+    })
+    .catch((error) => {
+      console.log(error.message);
+      process.exit(1);
+    });
+};
+
+connectToDB();
